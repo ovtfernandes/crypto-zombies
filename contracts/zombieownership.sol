132fca40f224db3c99bfa5bdde5072dc79bfe064
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-04 18:48:18 -0300

    Adds transfer functions implementations

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -5,6 +5,8 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+    mapping (uint => address) zombieApprovals;
+
     function balanceOf(address _owner) override external view returns (uint256) {
         return ownerZombieCount[_owner];
     }
@@ -13,12 +15,21 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         return zombieToOwner[_tokenId];
     }
 
-    function transferFrom(address _from, address _to, uint256 _tokenId) override external payable {
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
 
+    function transferFrom(address _from, address _to, uint256 _tokenId) override external payable {
+        require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
     }
 
     function approve(address _approved, uint256 _tokenId) override external payable {
-
+        zombieApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
     }
 
 }
