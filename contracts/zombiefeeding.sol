0822a99da0a9272e1486237d4cc8d9307fcc0530
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-04 16:39:22 -0300

    Adds kitty interface from crypto kitties

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -1,8 +1,12 @@
 pragma solidity >=0.7.0;
 
 import "./zombiefactory.sol";
+import "./kittyinterface.sol";
 
 contract ZombieFeeding is ZombieFactory {
+
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
