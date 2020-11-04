c95b07e3eea45ea42eebc418bcb117fa3b08751c
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-04 18:46:30 -0300

    Adds balanceOf and ownerOf implementations

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -6,11 +6,11 @@ import "./erc721.sol";
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
     function balanceOf(address _owner) override external view returns (uint256) {
-        
+        return ownerZombieCount[_owner];
     }
 
     function ownerOf(uint256 _tokenId) override external view returns (address) {
-        
+        return zombieToOwner[_tokenId];
     }
 
     function transferFrom(address _from, address _to, uint256 _tokenId) override external payable {
