b3d0e5ec91e6cfcadc70f2c35e5983ce5d0035ee
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-04 13:32:11 -0300

    Adds _createZombie function

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -12,4 +12,8 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
 }
