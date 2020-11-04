fd489e1f583b457db008a7b535f89da045af8b1f
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-11-04 18:25:48 -0300

    Adds pseudo random function

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -4,4 +4,11 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
 
+    uint randNonce = 0;
+  
+    function randMod(uint _modulus) internal returns(uint) {
+        randNonce++;
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
+    }
+
 }
